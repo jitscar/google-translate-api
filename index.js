72ecdf0334c962674b4c13babeb395b2a578c6e7
Yaroslav Agarkov <jitscar@gmail.com>
2018-06-01 15:05:41 +0300

    upd proxy

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ function translate(text, opts) {
         return got(url, { 
              agent: tunnel.httpOverHttp({
                 proxy: {
-                    host: '71.13.112.152',
-		    port: 3128
+                    host: '159.224.176.205',
+		    port: 53281
                 }
              })
         }).then(function (res) {
